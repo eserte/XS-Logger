feecbd6b051728cff0af2d8609bbe549e14aede5
Nicolas R <atoomic@cpan.org>
2018-01-16 10:13:38 -0600

    move to h

--- Logger.xs
+++ Logger.xs
@@ -14,26 +14,7 @@
 #endif
 
 /* c internal functions */
-/* this needs to go to our .h file */
 
-/* typedef enum { xfalse, xtrue } xbool; */
-
-typedef enum {
-		LOG_DEBUG, /* 0 */
-		LOG_INFO,  /* 1 */
-		LOG_WARN,  /* 2 */
-		LOG_ERROR, /* 3 or also DIE */
-		LOG_FATAL  /* 4 or also PANIC */
-} logLevel;
-
-typedef struct {
-	int x;
-	int y;
-	int fd; /* FIXME improve style -- maybe do not need to use is_open */
-	bool is_open;
-	char *filepath;
-	logLevel level;
-} MyLogger;
 
 /* function exposed to the module */
 /* maybe a bad idea to use a prefix */
--- logger.h
+++ logger.h
@@ -1,62 +1,30 @@
 /*
- * poll.h
+ * logger.h
  *
- * Copyright (c) 1997-8 Graham Barr <gbarr@pobox.com>. All rights reserved.
- * This program is free software; you can redistribute it and/or
- * modify it under the same terms as Perl itself.
  *
  */
 
-#ifndef POLL_H
-#  define POLL_H
+#ifndef XS_LOGGER_H
+#  define XS_LOGGER_H
 
-#if (defined(HAS_POLL) && defined(I_POLL)) || defined(POLLWRBAND)
-#  include <poll.h>
-#elif (defined(HAS_POLL) && defined(I_SYS_POLL))
-#  include <sys/poll.h>
-#else
-#ifdef HAS_SELECT
+/* typedef enum { xfalse, xtrue } xbool; */
 
+typedef enum {
+		LOG_DEBUG, /* 0 */
+		LOG_INFO,  /* 1 */
+		LOG_WARN,  /* 2 */
+		LOG_ERROR, /* 3 or also DIE */
+		LOG_FATAL  /* 4 or also PANIC */
+} logLevel;
 
-/* We shall emulate poll using select */
+typedef struct {
+	int x;
+	int y;
+	int pid;
+	int fd; /* FIXME improve style -- maybe do not need to use is_open */
+	bool is_open;
+	char *filepath;
+	logLevel level;
+} MyLogger;
 
-#define EMULATE_POLL_WITH_SELECT
-
-#ifdef poll
-# undef poll
-#endif
-#define poll Perl_my_poll
-
-typedef struct pollfd {
-    int fd;
-    short events;
-    short revents;
-} pollfd_t;
-
-#define	POLLIN		0x0001
-#define	POLLPRI		0x0002
-#define	POLLOUT		0x0004
-#define	POLLRDNORM	0x0040
-#define	POLLWRNORM	POLLOUT
-#define	POLLRDBAND	0x0080
-#define	POLLWRBAND	0x0100
-#define	POLLNORM	POLLRDNORM
-
-/* Return ONLY events (NON testable) */
-
-#define	POLLERR		0x0008
-#define	POLLHUP		0x0010
-#define	POLLNVAL	0x0020
-
-int poll (struct pollfd *, unsigned long, int);
-
-#ifndef HAS_POLL
-#  define HAS_POLL
-#endif
-
-#endif /* HAS_SELECT */
-
-#endif /* I_POLL */
-
-#endif /* POLL_H */
-
+#endif /* XS_LOGGER_H */
